f4d21a68813b865d2f56b0183c4a807be0df4f5a
Keletso Kgosienngwe <https://github.com/Keletso-Kgosienngwe>
2025-08-26 16:16:53 +0200

    first commit

---
+++ src/components/AboutSection.jsx
@@ -0,0 +1,98 @@
+import { Briefcase, Code, User } from "lucide-react";
+
+export const AboutSection = () => {
+  return (
+    <section id="about" className="py-24 px-4 relative">
+      {" "}
+      <div className="container mx-auto max-w-5xl">
+        <h2 className="text-3xl md:text-4xl font-bold mb-12 text-center">
+          About <span className="text-primary"> Me</span>
+        </h2>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
+          <div className="space-y-6">
+            <h3 className="text-2xl font-semibold">
+              Passionate Data Analyst & Web Developer
+            </h3>
+
+            <p className="text-muted-foreground">
+              I have a BSc (Hons) in Business Intelligence and Data Analytics.
+              Proficient in data analysis, visualization, and reporting using
+              tools like Power BI, Python, R-programming and SQL. Additionally,
+              I have experience with web technologies like HTML, CSS, and
+              JavaScript.
+            </p>
+
+            <p className="text-muted-foreground">
+              I'm a curious and driven data analyst passionate about using data
+              to make better business decisions. I’ve worked on projects
+              involving predictive analytics, dashboards, and market insights,
+              and I’m now seeking opportunities to grow.
+            </p>
+
+            <div className="flex flex-col sm:flex-row gap-4 pt-4 justify-center">
+              <a href="#contact" className="cosmic-button">
+                {" "}
+                Get In Touch
+              </a>
+
+              <a
+                href="Resume_Keletso.pdf"
+                download="Resume_Keletso.pdf"
+                className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
+              >
+                Download CV
+              </a>
+            </div>
+          </div>
+
+          <div className="grid grid-cols-1 gap-6">
+            <div className="gradient-border p-6 card-hover">
+              <div className="flex items-start gap-4">
+                <div className="p-3 rounded-full bg-primary/10">
+                  <Code className="h-6 w-6 text-primary" />
+                </div>
+                <div className="text-left">
+                  <h4 className="font-semibold text-lg"> Web Development</h4>
+                  <p className="text-muted-foreground">
+                    Creating responsive websites and web applications with
+                    modern frameworks.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="gradient-border p-6 card-hover">
+              <div className="flex items-start gap-4">
+                <div className="p-3 rounded-full bg-primary/10">
+                  <User className="h-6 w-6 text-primary" />
+                </div>
+                <div className="text-left">
+                  <h4 className="font-semibold text-lg">Data Visualization & Reporting</h4>
+                  <p className="text-muted-foreground">
+                  Developing interactive dashboards and reports with tools such as Power BI and Tableau to track KPIs, 
+                  monitor performance, and enable data-driven decision-making.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="gradient-border p-6 card-hover">
+              <div className="flex items-start gap-4">
+                <div className="p-3 rounded-full bg-primary/10">
+                  <Briefcase className="h-6 w-6 text-primary" />
+                </div>
+
+                <div className="text-left">
+                  <h4 className="font-semibold text-lg">Database Management</h4>
+                  <p className="text-muted-foreground">
+                 Building and managing databases with SQL, Oracle, and MySQL to ensure accurate data storage, retrieval, 
+                 and performance optimization.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
